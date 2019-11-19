705e70d5f236ba6ace586cf74a0855ac8cfa8085
flatsiedatsie <unfold@gmail.com>
2019-11-19 22:04:44 +0000

    0.0.1

--- package.sh
+++ package.sh
@@ -4,8 +4,8 @@ version=$(grep '"version":' manifest.json | cut -d: -f2 | cut -d\" -f2)
 
 rm -rf SHA256SUMS package
 mkdir package
-cp *.py manifest.json LICENSE README.md package/
-cp -r pkg css js package/
+cp manifest.json LICENSE README.md package/
+cp -r css js package/
 cd package
 find . -type f \! -name SHA256SUMS -exec sha256sum {} \; >> SHA256SUMS
 cd ..
