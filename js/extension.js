5d2b47e12164a98d2e598e68f1c039275cce0b1e
flatsiedatsie <unfold@gmail.com>
2020-07-23 18:57:01 +0200

    1.0.1
    - Adds abilty to overlay logs on top of each other
    - Minor cosmetic improvements to thing overview page

--- js/extension.js
+++ js/extension.js
@@ -57,11 +57,13 @@
       		console.log(this);
 			//this.updateInputValue(thermostat.id, -1);
 			const list = document.getElementById('square-theme-log-list-ul');
+			const buttons = document.getElementById('square-theme-log-list-buttons');
 			if(!list){
 				this.addLogSelector();
 			}
 			else{
 				list.parentNode.removeChild(list);
+				buttons.parentNode.removeChild(buttons);
 			}
       	});
 
@@ -119,147 +121,167 @@
         }
         console.log("log item names = " + log_names);
         //if(log_names.length > 2 ){
-            
-            const logs_view = document.getElementById("logs-view");
-            logs_view.classList.add('long-list');
+        
+        const logs_view = document.getElementById("logs-view");
 
-            
+        
 
-			// Clear the log list
-			const log_list_container = document.getElementById("square-theme-log-list-container");
+		// Clear the log list
+		const log_list_container = document.getElementById("square-theme-log-list-container");
+		
+		log_list_container.innerHTML = "";
+
+        // Create checkbox list
+        let ul = document.createElement('ul');
+        ul.setAttribute("id", "square-theme-log-list-ul");
+
+
+        log_names.forEach(function (log_name) {
+            console.log(log_name);
+            let li = document.createElement('li');
+
+            //li.innerHTML += log_name;
+
+            var input = document.createElement("input");
+                            //input.type = "checkbox";
+                            input.setAttribute('type', 'checkbox');
+                            input.setAttribute('name', log_name);
+                            input.setAttribute('id', log_name);
+                            input.setAttribute('id', log_name);
+                            //input.value = log_name;
+            
+            var label = document.createElement("label");
+                            label.setAttribute("for",log_name);
+							
+			var span = document.createElement("span");
+							span.innerHTML = log_name;
+
+            label.appendChild(input);
+			label.appendChild(span);
+            li.appendChild(label);
 			
-			log_list_container.innerHTML = "";
+	  	    li.onclick = function(element_name){
+	  		    console.log("Logs list item clicked");
+	            //console.log(element_name);
+				//console.log(element_name.target.name);
+	  		  	//addLogSelector();
+				
+				
+				/*
+				var values = [];
+				var cbs = document.forms['test'].elements['colors'];
+				for(var i=0,cbLen=cbs.length;i<cbLen;i++){
+				  if(cbs[i].checked){
+				    values.push(cbs[i].value);
+				  } 
+				}
+				alert('You selected: ' + values.join(', '));
+				*/
+				
 
-            // Create checkbox list
-            let ul = document.createElement('ul');
-            ul.setAttribute("id", "square-theme-log-list-ul");
+				
+				/*
+				[].forEach.call(document.querySelectorAll('.logs-log-container'), function (el) {
+					console.log
+					el.style.visibility = 'hidden';
+				});
+				*/
+				
+				
+				// get list of all log names
+				const all_log_name_elements = document.querySelectorAll('#logs-view .logs-log-name');
+                var all_log_names = [];
+                for (const log_name_element of all_log_name_elements) {
+                    all_log_names.push(log_name_element.innerHTML);
+					console.log(log_name_element);
+					console.log(log_name_element.innerHTML);
+                }
+				console.log("all log item names = " + all_log_names);
+				
+				// get list of selected log names
+                const selected_logs = document.querySelectorAll(' #logs-view #square-theme-log-list-ul input:checked');
+                var selected_log_names = [];
+                for (const selected_log of selected_logs) {
+                    selected_log_names.push(selected_log.name);
+					console.log(selected_log);
+                }
+                console.log("selected log item names = " + selected_log_names);
+				
+				
+				
 
-
-            log_names.forEach(function (log_name) {
-                console.log(log_name);
-                let li = document.createElement('li');
-
-                //li.innerHTML += log_name;
-
-                var input = document.createElement("input");
-                                //input.type = "checkbox";
-                                input.setAttribute('type', 'checkbox');
-                                input.setAttribute('name', log_name);
-                                input.setAttribute('id', log_name);
-                                input.setAttribute('id', log_name);
-                                //input.value = log_name;
+				
+                //const all_names = document.querySelectorAll(' #logs-view .logs-log-name');
                 
-                var label = document.createElement("label");
-                                label.setAttribute("for",log_name);
-								
-				var span = document.createElement("span");
-								span.innerHTML = log_name;
-
-                label.appendChild(input);
-				label.appendChild(span);
-                li.appendChild(label);
-				
-		  	    li.onclick = function(element_name){
-		  		    console.log("Logs list item clicked");
-		            //console.log(element_name);
-					//console.log(element_name.target.name);
-		  		  	//addLogSelector();
+				/*
+				var all_log_names = [];
+                for (const item of all_names) {
 					
-					
-					/*
-					var values = [];
-					var cbs = document.forms['test'].elements['colors'];
-					for(var i=0,cbLen=cbs.length;i<cbLen;i++){
-					  if(cbs[i].checked){
-					    values.push(cbs[i].value);
-					  } 
+					if( all_names.indexOf(item.innerHTML) > -1 ){
+						
 					}
-					alert('You selected: ' + values.join(', '));
-					*/
-					
-
-					
-					/*
-					[].forEach.call(document.querySelectorAll('.logs-log-container'), function (el) {
-						console.log
-						el.style.visibility = 'hidden';
-					});
-					*/
-					
-					
-					// get list of all log names
-					const all_log_name_elements = document.querySelectorAll('#logs-view .logs-log-name');
-	                var all_log_names = [];
-	                for (const log_name_element of all_log_name_elements) {
-	                    all_log_names.push(log_name_element.innerHTML);
-						console.log(log_name_element);
-						console.log(log_name_element.innerHTML);
-	                }
-					console.log("all log item names = " + all_log_names);
-					
-					// get list of selected log names
-	                const selected_logs = document.querySelectorAll(' #logs-view #square-theme-log-list-ul input:checked');
-	                var selected_log_names = [];
-	                for (const selected_log of selected_logs) {
-	                    selected_log_names.push(selected_log.name);
-						console.log(selected_log);
-	                }
-	                console.log("selected log item names = " + selected_log_names);
-					
-					
-					
-
-					
-	                //const all_names = document.querySelectorAll(' #logs-view .logs-log-name');
-	                
-					/*
-					var all_log_names = [];
-	                for (const item of all_names) {
+					else{
 						
-						if( all_names.indexOf(item.innerHTML) > -1 ){
-							
-						}
-						else{
-							
-						}
-						
-	                    log_names.push(item.innerHTML);
-	                }
-					*/
-					
-					
-					const all_logs = document.querySelectorAll(' #logs-view .logs-log-container');
-					
-					var log_counter = 0;
-					for (const log_container of all_logs) {
-
-						
-						// If the current corresponding name is in the selected arrays name
-						
-						
-						if( selected_log_names.indexOf(all_log_names[log_counter]) > -1 || selected_log_names.length == 0 ){
-							console.log("do not hide " + all_log_names[log_counter]);
-							//log_container.style.visibility = 'visible';
-							log_container.style.display = 'block';
-						}
-						else{
-							console.log("hiding log container: " + all_log_names[log_counter]);
-							//console.log(log_container);
-							//log_container.style.visibility = 'hidden';
-							log_container.style.display = 'none';
-						}
-						log_counter++;
 					}
 					
-					
-					
-		  	  	}
+                    log_names.push(item.innerHTML);
+                }
+				*/
 				
-                ul.appendChild(li);
+				
+				const all_logs = document.querySelectorAll(' #logs-view .logs-log-container');
+				
+				var log_counter = 0;
+				for (const log_container of all_logs) {
 
-            });
+					
+					// If the current corresponding name is in the selected arrays name
+					if( selected_log_names.indexOf(all_log_names[log_counter]) > -1 || selected_log_names.length == 0 ){
+						console.log("do not hide " + all_log_names[log_counter]);
+						//log_container.style.visibility = 'visible';
+						log_container.style.display = 'block';
+					}
+					else{
+						console.log("hiding log container: " + all_log_names[log_counter]);
+						//console.log(log_container);
+						//log_container.style.visibility = 'hidden';
+						log_container.style.display = 'none';
+					}
+					log_counter++;
+				}
+				
+				
+				
+	  	  	}
+			
+            ul.appendChild(li);
 
-            log_list_container.appendChild(ul);	
+        });
+
+        log_list_container.appendChild(ul);	
+		
+		
+		let filter_buttons = document.createElement('div');
+		filter_buttons.setAttribute("id", "square-theme-log-list-buttons");
+		
+		
+		let overlay_button = document.createElement('button');
+		overlay_button.setAttribute("id", "square-theme-logs-overlay-button");
+		overlay_button.textContent = "Overlay";
+		filter_buttons.appendChild(overlay_button)
+		log_list_container.appendChild(filter_buttons)
+		
+		document.getElementById("square-theme-logs-overlay-button").onclick = function(element_name){
+  		    console.log("Overlay button clicked");
+			
+	        if (logs_view.classList.contains('square-theme-logs-overlay')) {
+				logs_view.classList.remove('square-theme-logs-overlay');
+		  	}
+			else{
+				logs_view.classList.add('square-theme-logs-overlay');
+			}
+			
+		}
 			
 		//}
 		
