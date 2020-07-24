f148508e847c8888013ad6dee9848b7bbef2a56c
Michael Stegeman <michael@stegeman.me>
2020-07-24 12:31:41 -0800

    Clean up packaging.

--- package.sh
+++ package.sh
@@ -1,15 +1,32 @@
-#!/bin/bash
+#!/bin/bash -e
 
-version=$(grep '"version":' manifest.json | cut -d: -f2 | cut -d\" -f2)
+version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
 
-rm -rf SHA256SUMS package
-rm -rf *.tgz
+# Clean up from previous releases
+rm -rf *.tgz package SHA256SUMS
+
+# Prep new package
 mkdir package
+
+# Put package together
 cp manifest.json LICENSE README.md package/
 cp -r css js package/
-cd package
-find . -type f \! -name SHA256SUMS -exec sha256sum {} \; >> SHA256SUMS
-cd ..
+find package -type f -name '*.pyc' -delete
+find package -type f -name '._*' -delete
+find package -type d -empty -delete
 
-tar czf "square-theme-${version}.tgz" package
-sha256sum "square-theme-${version}.tgz"
+# Generate checksums
+echo "generating checksums"
+cd package
+find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+cd -
+
+# Make the tarball
+echo "creating archive"
+TARFILE="square-theme-${version}.tgz"
+tar czf ${TARFILE} package
+
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+cat ${TARFILE}.sha256sum
+
+rm -rf SHA256SUMS package
